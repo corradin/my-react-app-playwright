14064f531dd540ba3da20ac2d944d62d332ecca9
Riccardo Corradin <riccardo.corradin@live.com>
2020-06-16 13:11:00 +0200

    Added current time

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,66 @@ import logo from './logo.svg';
 import './App.css';
 import CSS from 'csstype';
 
-function App() {
-  const strangeLinkStyle: CSS.Properties = {
-    display: 'flex',
-    alignItems: 'baseline',
-    width: '420px'
-  };
+type ClockState = {
+  time: Date
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {/* Strangely styled link */}
-        <div style={strangeLinkStyle}>
-          <a href="http://www.google.com" id="unclickable_link">
-            OK
+class App extends React.Component<{}, ClockState> {
+  intervalID: NodeJS.Timeout = setTimeout(() => {}, 0);
+
+  constructor(props: Readonly<App>) {
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    const strangeLinkStyle: CSS.Properties = {
+      display: 'flex',
+      alignItems: 'baseline',
+      width: '420px',
+    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          {/* Strangely styled link */}
+          <div style={strangeLinkStyle}>
+            <a href="http://www.google.com" id="unclickable_link">
+              OK
+            </a>
+          </div>
+          <p>
+            The time is {this.state.time.toLocaleString()}.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
           </a>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
